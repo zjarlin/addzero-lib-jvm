d8500e41884c948f16fa64eecd40930aa06a89a7
zjarlin <zjarlin@outlook.com>
2025-08-31 00:34:35 +0800

    change

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,7 +1,6 @@
 @file:OptIn(ExperimentalWasmDsl::class)
 
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     id("kmp-compose-common")
