165c47d09793436e6bebfda4e1c3087e74cb6ebe
zjarlin <zjarlin@outlook.com>
2025-09-15 22:11:17 +0800

    change

--- buildSrc/src/main/kotlin/addzero/conventions/addzero-component.gradle.kts
+++ buildSrc/src/main/kotlin/addzero/conventions/addzero-component.gradle.kts
@@ -1,16 +1,14 @@
-import org.gradle.platform.base.internal.DefaultBinaryNamingScheme.component
-
 plugins {
     id("org.jetbrains.kotlin.multiplatform")
 }
-
 // 自动查找所有 addzero-compose-native-component- 开头的子模块
 val componentModules = rootProject.subprojects
-    .filter { it.name.startsWith("addzero-compose-native-component-") &&
-     it.name != "addzero-compose-native-component"
+    .filter {
+        it.name.startsWith("addzero-compose-native-component-") &&
+                it.name != "addzero-compose-native-component"
 //            &&
 //        it.name != "addzero-compose-native-component-table-pro"
-     }
+    }
     .map { it }
 //componentModules.forEach {
 //    println("揭晓的模块${it.name}")
