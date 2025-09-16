6854e92b1ae54926dc11d71d7311d4d7bb624f2f
zjarlin <zjarlin@outlook.com>
2025-09-16 20:44:18 +0800

    change

--- lib/gradle-plugin/addzero-gradle-ksp-buddy/build.gradle.kts
+++ lib/gradle-plugin/addzero-gradle-ksp-buddy/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
-//    kotlin("plugin.serialization") version "1.9.10"
 }
 
 repositories {
@@ -11,13 +10,11 @@ repositories {
 
 dependencies {
     implementation(gradleApi())
-//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
 
 gradlePlugin {
     plugins {
         create(project.name) {
-
             id = "ksp-buddy"
             implementationClass = "site.addzero.gradle.plugin.kspbuddy.KspBuddyPlugin"
             displayName = "KSP Buddy Plugin"
