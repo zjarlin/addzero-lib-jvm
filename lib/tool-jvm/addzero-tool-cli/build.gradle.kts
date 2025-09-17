1738e8214f883545272c3f714443451b28b3597c
zjarlin <zjarlin@outlook.com>
2025-09-17 22:51:51 +0800

    change

--- lib/tool-jvm/addzero-tool-cli/build.gradle.kts
+++ lib/tool-jvm/addzero-tool-cli/build.gradle.kts
@@ -8,20 +8,17 @@ plugins {
     id("kmp-koin-core")
     id("ksp4self")
 }
+
 kotlin {
     macosX64()
     macosArm64()
     mingwX64()
     linuxX64()
     linuxArm64()
-    dependencies{
+
+    dependencies {
+        implementation(libs.kotlinx.coroutines.core)
+
     }
+
 }
-
-
-//application {
-//    // Define the Fully Qualified Name for the application main class
-//    // (Note that Kotlin compiles `Main.kt` to a class with FQN `com.example.app.MainKt`.)
-//    mainClass = "site.addzero.app.MainKt"
-//}
-
