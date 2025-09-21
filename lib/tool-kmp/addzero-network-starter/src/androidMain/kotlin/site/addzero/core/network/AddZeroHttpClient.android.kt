2747879533ef2b3138a15ae149e9595b035753bb
zjarlin <zjarlin@outlook.com>
2025-09-21 20:42:20 +0800

    change

--- lib/tool-kmp/addzero-network-starter/src/androidMain/kotlin/site/addzero/core/network/AddZeroHttpClient.android.kt
+++ lib/tool-kmp/addzero-network-starter/src/androidMain/kotlin/site/addzero/core/network/AddZeroHttpClient.android.kt
@@ -3,5 +3,5 @@ package site.addzero.core.network
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 
-actual val apiClient: io.ktor.client.HttpClient
+actual val apiClient: HttpClient
    get() = HttpClient(CIO, configClient())
