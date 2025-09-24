512967756b26383bb9f04a89f8a08a92a085bda8
zjarlin <zjarlin@outlook.com>
2025-09-24 15:58:25 +0800

    change

--- backend/server/src/main/kotlin/site/addzero/web/modules/controller/product/DeviceController.kt
+++ backend/server/src/main/kotlin/site/addzero/web/modules/controller/product/DeviceController.kt
@@ -2,12 +2,10 @@ package site.addzero.web.modules.controller.product
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import site.addzero.model.entity.biz_device.*
+import site.addzero.model.entity.biz_device.Device
 import site.addzero.web.infra.jimmer.base.BaseController
 
 @RestController
-@RequestMapping("/product")
-class ProductController : BaseController<Product> {
+@RequestMapping("/device")
+class DeviceController : BaseController<Device> {
 }
-
-
--- backend/server/src/main/kotlin/site/addzero/web/modules/controller/product/ProductCategoryController.kt
+++ backend/server/src/main/kotlin/site/addzero/web/modules/controller/product/ProductCategoryController.kt
@@ -2,12 +2,9 @@ package site.addzero.web.modules.controller.product
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import site.addzero.model.entity.biz_device.*
+import site.addzero.model.entity.biz_device.ProductCategory
 import site.addzero.web.infra.jimmer.base.BaseController
 
 @RestController
-@RequestMapping("/product")
-class ProductController : BaseController<Product> {
-}
-
-
+@RequestMapping("/productCategory")
+class ProductCategoryController : BaseController<ProductCategory>
