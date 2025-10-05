8be4caaa0a563e01246454c58e0500d6594d7b26
zjarlin <zjarlin@outlook.com>
2025-10-05 12:00:18 +0800

    change

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ version = versionDate.toString()
 subprojects {
 //    println("aaaaaaaaa$path")
     if (path.startsWith(":lib:")) {
+//        println("aaaaaaa$name")
         apply(plugin = "site.addzero.publish-buddy")
     }
 }
