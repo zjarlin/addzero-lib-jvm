2cf3f99a34b9e3e8bb7acc674f1f46798a98dd64
zjarlin <zjarlin@outlook.com>
2025-10-16 17:00:08 +0800

    change

--- build-logic/src/main/kotlin/site/addzero/spring/spring-common.gradle.kts
+++ build-logic/src/main/kotlin/site/addzero/spring/spring-common.gradle.kts
@@ -1,6 +1,21 @@
 package site.addzero.spring
 
-import site.addzero.gradle.getLibs
+import org.gradle.api.provider.Property
+
+// 默认配置
+private val springBootVersion = "2.7.18"
+private val springBom = "org.springframework.boot:spring-boot-dependencies"
+
+interface SpringCommonExtension {
+    val version: Property<String>
+}
+
+val create = extensions.create<SpringCommonExtension>("addzeroSpringBuddy").apply {
+    version.set(springBootVersion)
+}
+
+// 计算属性
+val version: String get() = create.version.get()
 
 plugins {
     id("site.addzero.jvm.kotlin-convention")
@@ -9,7 +24,7 @@ plugins {
 }
 
 dependencies {
-    implementation(platform(getLibs().spring.bom))
+    implementation(platform("$springBom:$version"))
 }
 
 
--- build-logic/src/main/kotlin/site/addzero/spring/spring-starter.gradle.kts
+++ build-logic/src/main/kotlin/site/addzero/spring/spring-starter.gradle.kts
@@ -1,26 +1,18 @@
 package site.addzero.spring
 
-import site.addzero.gradle.getLibs
-
 plugins {
     id("site.addzero.spring.spring-common")
 }
 
-//java {
-//    toolchain {
-//        languageVersion.set(JavaLanguageVersion.of(8))
-//    }
-//}
-//
-//kotlin {
-//    jvmToolchain(8)
-//}
-
 dependencies {
     implementation("org.springframework:spring-core")
     implementation("org.springframework:spring-context")
+
+    // 自动配置
     implementation("org.springframework.boot:spring-boot-autoconfigure")
-    implementation("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    // 元数据生成
+    compileOnly("org.springframework.boot:spring-boot-autoconfigure-processor")
 //    compileOnly("org.aspectj:aspectjweaver:1.9.9")
     compileOnly("org.aspectj:aspectjweaver")
 }
