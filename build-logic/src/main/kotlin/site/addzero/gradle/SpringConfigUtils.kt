7042ed53dada8d4713fcd29551f539eff0db8339
zjarlin <zjarlin@outlook.com>
2025-10-16 18:16:10 +0800

    change

--- build-logic/src/main/kotlin/site/addzero/gradle/SpringConfigUtils.kt
+++ build-logic/src/main/kotlin/site/addzero/gradle/SpringConfigUtils.kt
@@ -1,11 +1,5 @@
 package site.addzero.gradle
 
-import org.gradle.api.provider.Property
-
-//interface AdzeroSpringBuddyExtension {
-//    var version: Property<String>
-//}
-
 interface AdzeroSpringBuddyExtension {
     var version: String
 }
