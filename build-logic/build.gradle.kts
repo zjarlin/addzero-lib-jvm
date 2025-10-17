d49a13285b76672d21e69137f1b97b31715b4935
zjarlin <zjarlin@outlook.com>
2025-10-17 11:16:29 +0800

    change

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -12,22 +12,17 @@ repositories {
     gradlePluginPortal()
 }
 
-
 dependencies {
     gradleApi()
     compileOnly(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
-    compileOnly("org.yaml:snakeyaml:+")
-    compileOnly("org.jetbrains.kotlin:kotlin-serialization:+")
-    compileOnly("org.jetbrains.compose:compose-gradle-plugin:+")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:+")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:+")
-    implementation("com.vanniktech:gradle-maven-publish-plugin:+")
-    implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:+")
-
-
-    implementation("io.spring.gradle:dependency-management-plugin:1.1.7")
-    implementation("org.springframework.boot:org.springframework.boot.gradle.plugin:2.7.18")
-    implementation("org.jetbrains.kotlin.plugin.spring:org.jetbrains.kotlin.plugin.spring.gradle.plugin:2.1.20")
-
-
+    compileOnly(libs.gradlePlugin.jetbrainsCompose)
+    compileOnly(libs.gradlePlugin.kotlinSerialization)
+    implementation(libs.gradlePlugin.dokka)
+    implementation(libs.gradlePlugin.kotlin)
+    implementation(libs.gradlePlugin.mavenPublish)
+    implementation(libs.org.graalvm.buildtools.native.gradle.plugin)
+    
+    implementation(libs.gradlePlugin.dependencyManagement)
+    implementation(libs.gradlePlugin.springBoot)
+    implementation(libs.gradlePlugin.kotlinSpring)
 }
