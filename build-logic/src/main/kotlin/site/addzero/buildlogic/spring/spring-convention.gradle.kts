9f75f0e49b286e7450bd85172a71cf97d75f6c15
zjarlin <zjarlin@outlook.com>
2025-10-17 15:36:07 +0800

    change

--- build-logic/src/main/kotlin/site/addzero/buildlogic/spring/spring-convention.gradle.kts
+++
@@ -1,9 +0,0 @@
-package site.addzero.buildlogic.spring
-
-plugins {
-    id("site.addzero.buildlogic.spring.spring-common")
-    id("org.springframework.boot")
-}
-
-
-
--- build-logic/src/main/kotlin/site/addzero/gradle/ExtensionDef.kt
+++
@@ -1,9 +0,0 @@
-package site.addzero.gradle
-
-import org.gradle.api.provider.Property
-
-interface AdzeroExtension {
-    val jdkVersion: Property<String>
-    val springVersion: Property<String>
-}
-
