6bc3b59227343552c3bcbf28e1fab5ca68202c0b
zjarlin <zjarlin@outlook.com>
2025-10-20 09:58:22 +0800

    change

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,8 @@ rootProject.name = rootDir.name
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 plugins {
-//    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("site.addzero.repo-buddy") version "+"
     id("org.gradle.toolchains.foojay-resolver-convention") version "+"
+    id("site.addzero.repo-buddy") version "+"
     id("site.addzero.modules-buddy") version "+"
     id("me.champeau.includegit") version "+"
 }
@@ -18,12 +17,7 @@ gitRepositories {
         uri.set("https://gitee.com/zjarlin/addzero-lib-jvm-stable.git")
         branch.set("master")
     }
-
-
 }
-//autoModules{
-//   excludeModules=arrayOf("build-logic")
-//}
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
@@ -31,4 +25,3 @@ dependencyResolutionManagement {
         }
     }
 }
-includeBuild("checkouts/build-logic")
