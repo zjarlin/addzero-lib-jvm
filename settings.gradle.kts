ed3b63469cdc3827812f1415500ec63a63e799b8
zjarlin <zjarlin@outlook.com>
2025-10-23 21:26:31 +0800

    change

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,16 +5,16 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "+"
     id("site.addzero.repo-buddy") version "+"
-    id("site.addzero.modules-buddy") version "0.0.652"
+//    id("site.addzero.modules-buddy") version "0.0.652"
+
+    id("io.gitee.zjarlin.auto-modules") version "0.0.608"
     id("me.champeau.includegit") version "+"
 }
 
-//autoModules {
-//    excludeModules.convention
-//    preidicate={
-//        true
-//    }
-//}
+autoModules {
+    excludeModules=listOf("build-logic","addzero-lib-jvm-stable")
+}
+includeBuild("checkouts/build-logic")
 gitRepositories {
     include("build-logic") {
         uri.set("https://gitee.com/zjarlin/build-logic.git")
