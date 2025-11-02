21bc26d986a92004fe72d05a4cd516b7090c7e37
zjarlin <zjarlin@outlook.com>
2025-11-02 15:24:56 +0800

    change

--- lib/idea-plugin/autoddl/src/main/kotlin/site/addzero/ide/init/AutoDDLConfigRegistrar.kt
+++ lib/idea-plugin/autoddl/src/main/kotlin/site/addzero/ide/init/AutoDDLConfigRegistrar.kt
@@ -1,17 +1,12 @@
 package site.addzero.ide.init
 
-import com.intellij.codeInsight.intention.IntentionManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import site.addzero.ide.config.ui.AutoDDLConfigurable
 
 class AutoDDLConfigRegistrar : ProjectActivity {
     override suspend fun execute(project: Project) {
-        registerIntentions()
-//        AutoDDLConfigurableProvider()
-
-                AutoDDLConfigurable()
-
+        // AutoDDLConfigurable 会通过 plugin.xml 中的配置自动注册
+        // 这里不需要额外的代码来启用 settings UI
     }
 }
-
