e195fa9add180b75faf8d52e01b0d57fd2a56246
zjarlin <zjarlin@outlook.com>
2025-11-02 10:25:24 +0800

    change

---
+++ lib/idea-plugin/autoddl/src/main/kotlin/site/addzero/ide/config/model/InputType.kt
@@ -0,0 +1,11 @@
+package site.addzero.ide.config.model
+
+/**
+ * 配置字段的输入类型枚举
+ */
+enum class InputType {
+    TEXT,       // 文本输入
+    NUMBER,     // 数字输入
+    PASSWORD,   // 密码输入
+    TEXTAREA    // 多行文本输入
+}
