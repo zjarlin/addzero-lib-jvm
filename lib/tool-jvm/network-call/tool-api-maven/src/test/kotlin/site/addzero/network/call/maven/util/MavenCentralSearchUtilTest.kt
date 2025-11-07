cd268b58ed28500ca3c44286d3ddacb7aaf04969
zjl <648887879@qq.com>
2025-11-07 17:29:40 +0800

    change

--- lib/tool-jvm/network-call/tool-api-maven/src/test/kotlin/site/addzero/network/call/maven/util/MavenCentralSearchUtilTest.kt
+++ lib/tool-jvm/network-call/tool-api-maven/src/test/kotlin/site/addzero/network/call/maven/util/MavenCentralSearchUtilTest.kt
@@ -2,29 +2,36 @@ package site.addzero.network.call.maven.util
 
 import org.junit.jupiter.api.Test
 
+private const val IO_GITHUB_ZEPHYRCICD = "io.github.zephyrcicd"
+
 class MavenCentralSearchUtilTest {
 
     @Test
     fun `should search artifacts by group id with new API`() {
-        val artifacts = MavenCentralSearchUtil.searchByGroupId("site.addzero", 5)
-//        assertTrue(artifacts.isNotEmpty(), "Should find artifacts for group id")
-        println("Found ${artifacts.size} artifacts for group 'org.babyfish.jimmer'")
-        artifacts.forEach {
-            println("  ${it.groupId}:${it.artifactId} (${it.latestVersion})")
-        }
+        val artifacts = MavenCentralSearchUtil.searchByGroupId(IO_GITHUB_ZEPHYRCICD, 5)
+        println()
+
     }
 
     @Test
     fun `should get latest version by group id with new API`() {
-        val latestVersion = MavenCentralSearchUtil.getLatestVersionByGroupId("org.babyfish.jimmer")
-        val latestVersion1 = MavenCentralSearchUtil.getLatestVersionByGroupId("site.addzero")
+//        val latestVersion = MavenCentralSearchUtil.getLatestVersionByGroupId("org.babyfish.jimmer")
+//        val latestVersion1 = MavenCentralSearchUtil.getLatestVersionByGroupId("site.addzero")
+        val latestVersion12 = MavenCentralSearchUtil .getLatestVersionByGroupId(IO_GITHUB_ZEPHYRCICD)
 //        assertNotNull(latestVersion, "Should get latest version for group id")
-        println("Latest version in group 'org.babyfish.jimmer' is $latestVersion")
+//        println("Latest version in group 'org.babyfish.jimmer' is $latestVersion")
+        println()
     }
 
     @Test
     fun `should search artifacts for jackson group with new API`() {
         val artifacts = MavenCentralSearchUtil.searchByGroupId("site.addzero", 5)
         println()
+    }
+    @Test
+    fun `search by ati`(): Unit {
+        val searchByArtifactId = MavenCentralSearchUtil.searchByArtifactId("tool-api-maven", 5)
+        println()
+
     }
 }
