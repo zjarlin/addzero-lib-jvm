18c1d0aeef9949ec3f95e2aceff50c93db9739d2
zjl <648887879@qq.com>
2025-11-08 13:43:04 +0800

    change

--- lib/tool-jvm/tool-spring/src/main/kotlin/site/addzero/web/infra/spring/SprCtxUtil.kt
+++ lib/tool-jvm/tool-spring/src/main/kotlin/site/addzero/web/infra/spring/SprCtxUtil.kt
@@ -3,18 +3,19 @@ package site.addzero.web.infra.spring
 import cn.hutool.extra.spring.SpringUtil
 import org.springframework.core.ResolvableType
 
-/**
- * @Description: spring上下文工具类
- * @author: jeecg-boot
- */
-object SprCtxUtil {
-    inline fun <reified T> getBean(vararg generics: Class<*>): T? {
+    inline fun <reified T> getGenericBean(vararg generics: Class<*>): T? {
+    val bean = getBean(T::class.java, *generics)
+    return bean
+}
+    /**
+     * @param clazz Bean的类
+     * @param generics 泛型类型数组
+     * @param <T> Bean类型
+     * @return Bean实例或null（如果不存在）
+     */
+    fun <T> getBean(clazz: Class<T>, vararg generics: Class<*>): T? {
         val applicationContext = SpringUtil.getApplicationContext()
-        val resolvableType = ResolvableType.forClassWithGenerics(T::class.java, *generics)
+        val resolvableType = ResolvableType.forClassWithGenerics(clazz, *generics)
         val beanProvider = applicationContext.getBeanProvider<T>(resolvableType)
         return beanProvider.getIfAvailable()
     }
-
-
-
-}
