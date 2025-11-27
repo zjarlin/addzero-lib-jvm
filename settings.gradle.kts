556e715f143c5e35162d9299dd9c23f39f89255d
zjarlin <zjarlin@outlook.com>
2025-11-27 22:40:23 +0800

    chore(deps): 更新 Gradle 插件 ID
    - 将插件 ID 从 site.addzero.repo-buddy 更改为
      site.addzero.gradle.plugin.repo-buddy
    - 保持插件版本号不变
    - 注释掉 modules-buddy 和 auto-modules 插件配置
    - 维持原有的插件依赖结构

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "+"
-    id("site.addzero.repo-buddy") version "+"
+    id("site.addzero.gradle.plugin.repo-buddy") version "+"
 //    id("site.addzero.modules-buddy") version "0.0.652"
 
 //    id("io.gitee.zjarlin.auto-modules") version "0.0.608"
