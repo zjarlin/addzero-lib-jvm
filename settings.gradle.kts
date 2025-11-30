e48c7885d139eac80a3d44501497eb3f37c18ad1
zjarlin <zjarlin@outlook.com>
2025-11-30 14:32:50 +0800

    refactor(settings): 更新Gradle模块引用路径
    - 将gradle-checkout-repos模块重命名为gradle-git-dependency
    - 注释掉netty-util模块的引用配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -84,7 +84,7 @@ include(":lib:apt-dict-processor")
 //include(":lib:gradle-plugin:gradle-tool")
 //include(":lib:gradle-plugin:gradle-script")
 //include(":lib:gradle-plugin:settings-plugin:gradle-repo-buddy")
-include(":lib:gradle-plugin:settings-plugin:gradle-checkout-repos")
+include(":lib:gradle-plugin:settings-plugin:gradle-git-dependency")
 
 //include(":lib:gradle-plugin:settings-plugin:gradle-modules-buddy")
 //include(":lib:gradle-plugin:gradle-script-core")
