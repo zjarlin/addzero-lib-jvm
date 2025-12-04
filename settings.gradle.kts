c3071fbdb0dcaaa26437f0e2bdfda0141f693f7f
zjarlin <zjarlin@outlook.com>
2025-12-04 16:25:01 +0800

    feat(lsi): 支持类型可空性判断与懒加载优化
    - 引入 isNullable 属性，基于注解自动判断类型的可空性
    - 移除硬编码的 Java 可空类型判断逻辑
    - 新增 Nullable 与 NonNull 注解常量定义
    - 提供辅助函数 isNullable 用于注解驱动的可空性推断
    - 更新 LSI 模块结构，新增

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,10 +11,15 @@ implementationRemoteGit{
 }
 
 // >>> Gradle Buddy: On-Demand Modules (DO NOT EDIT THIS BLOCK) >>>
-// Generated at: 2025-12-04T14:17:04.751663
-// Loaded: 3, Excluded: 0, Total: 3
+// Generated at: 2025-12-04T16:09:59.387142
+// Loaded: 8, Excluded: 0, Total: 8
 include(":checkouts:metaprogramming-lsi:lsi-apt")
 include(":checkouts:metaprogramming-lsi:lsi-core")
+include(":checkouts:metaprogramming-lsi:lsi-intellij")
+include(":checkouts:metaprogramming-lsi:lsi-kt")
+include(":checkouts:metaprogramming-lsi:lsi-psi")
+include(":checkouts:metaprogramming-lsi:lsi-psiandkt")
+include(":checkouts:metaprogramming-lsi:lsi-reflection")
 include(":lib:apt-controller2feign-processor")
 // <<< Gradle Buddy: End Of Block <<<
 
