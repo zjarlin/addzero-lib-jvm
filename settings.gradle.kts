a20b6af053c3f20a820d874d27a58e789f23fa3f
zjarlin <zjarlin@outlook.com>
2026-01-31 11:20:14 +0800

    chore(build): 更新依赖版本并简化Gradle模块配置
    - 将kotlin版本从2.3.20-Beta1升级至2.3.20-Beta2
    - 将ksp版本从2.3.4升级至2.3.5
    - 精简settings.gradle.kts中的模块包含，仅保留必要模块

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,23 +9,8 @@ plugins {
 }
 
 // >>> Gradle Module Sleep: On-Demand Modules (DO NOT EDIT THIS BLOCK) >>>
-// Generated at: 2026-01-31T10:29:27.986344
-// Loaded: 17, Excluded: 0, Total: 17
-include(":lib:gradle-plugin:conventions:intellij-convention")
-include(":lib:gradle-plugin:conventions:jvm-conventions:graalvm-convention")
-include(":lib:gradle-plugin:conventions:jvm-conventions:java-convention")
-include(":lib:gradle-plugin:conventions:jvm-conventions:koin-convention")
-include(":lib:gradle-plugin:conventions:jvm-conventions:kotlin-convention")
-include(":lib:gradle-plugin:conventions:jvm-conventions:ksp-convention")
-include(":lib:gradle-plugin:conventions:jvm-conventions:ktxjson-convention")
-include(":lib:gradle-plugin:conventions:jvm-conventions:lombok-convention")
-include(":lib:gradle-plugin:conventions:jvm-conventions:spring-convention")
-include(":lib:gradle-plugin:conventions:kmp-conventions:kmp-convention")
-include(":lib:gradle-plugin:conventions:koin-convention")
-include(":lib:gradle-plugin:conventions:ksp-conventions")
-include(":lib:gradle-plugin:conventions:other-conventions")
-include(":lib:gradle-plugin:conventions:spring-conventions")
+// Generated at: 2026-01-31T10:51:00.856890
+// Loaded: 2, Excluded: 0, Total: 2
 include(":lib:gradle-plugin:project-plugin:gradle-processor-buddy")
-include(":lib:tool-jvm:database:ddlgenerator")
 include(":lib:tool-jvm:tool-yml")
 // <<< Gradle Module Sleep: End Of Block <<<
