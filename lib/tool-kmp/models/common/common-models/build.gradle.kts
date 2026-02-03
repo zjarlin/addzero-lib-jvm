238ae43a9329040c7881de2972ea1dc09da0d747
zjarlin <zjarlin@outlook.com>
2026-02-03 18:29:05 +0800

    refactor(build-logic): update plugin ID to `kmp-json-withtool` with full namespace

--- lib/tool-kmp/models/common/common-models/build.gradle.kts
+++ lib/tool-kmp/models/common/common-models/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("site.addzero.buildlogic.jvm.jvm-json-withtool")
+  id("site.addzero.buildlogic.kmp.composition.kmp-json-withtool")
   id("site.addzero.buildlogic.kmp.platform.kmp-core")
 }
 // 配置 Kotlin 编译器选项以启用新特性
