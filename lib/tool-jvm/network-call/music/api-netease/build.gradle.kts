dcc03b3d934f71cc07c57d82f4b50c042d0cc5b9
zjarlin <zjarlin@outlook.com>
2026-02-16 17:25:28 +0800

    chore(gradle, api-netease): activate `kmp-ksp-plugin` and clean up dependency formatting

--- lib/tool-jvm/network-call/music/api-netease/build.gradle.kts
+++ lib/tool-jvm/network-call/music/api-netease/build.gradle.kts
@@ -2,12 +2,12 @@ plugins {
   id("site.addzero.buildlogic.kmp.kmp-ktorfit")
 //    id("site.addzero.buildlogic.kmp.kmp-ktor-client")
   id("site.addzero.buildlogic.kmp.kmp-json-withtool")
-//    id("site.addzero.buildlogic.kmp.kmp-ksp-plugin")
+  id("site.addzero.buildlogic.kmp.kmp-ksp-plugin")
 }
 dependencies {
 //    kspCommonMainMetadata("io.github.ltttttttttttt:LazyPeopleHttp:2.2.5")
 //    kspCommonMainMetadata("io.github.ltttttttttttt:LazyPeopleHttp:2.2.5")
- kspCommonMainMetadata(libs.method.semanticizer.processor)
+  kspCommonMainMetadata(libs.method.semanticizer.processor)
 //  kspCommonMainMetadata(project(":lib:tool-jvm:network-call:music:api-netease-semantic-impl"))
 }
 version = "2026.02.16"
