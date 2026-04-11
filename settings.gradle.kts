e80058e3792243a1d87208b004a01f8c68c2b8e1
zjarlin <zjarlin@outlook.com>
2026-04-11 14:06:12 +0800

    chore(build): 添加settings.gradle.kts配置和模块包含列表
    - 设置rootProject名称为根目录名
    - 启用类型安全的项目访问预览功能
    - 应用自定义Gradle插件 site.addzero.gradle.plugin.repo-buddy 和 addzero-git-dependency
    - 添加模块配置注释用于按需模块加载
    - 包含26个子模块，涵盖ksp元数据、工具库以及启动器组件
    - 暂时注释modules-buddy插件申请

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,27 +9,32 @@ plugins {
 
 
 // >>> Gradle Module Sleep: On-Demand Modules (DO NOT EDIT THIS BLOCK) >>>
-// Generated at: 2026-04-11T09:47:10.743319
-// Loaded: 21, Excluded: 0, Total: 21
-include(":lib:compose:app-sidebar-cupertino-adapter")
+// Generated at: 2026-04-13T10:25:17.946030
+// Loaded: 26, Excluded: 0, Total: 26
 include(":lib:ksp:metadata:controller2api-smoke")
 include(":lib:ksp:metadata:entity2form:entity2form-processor")
-include(":lib:ksp:metadata:entity2iso-processor")
-include(":lib:ksp:metadata:modbus:modbus-codegen-core")
-include(":lib:ksp:metadata:modbus:modbus-codegen-model")
-include(":lib:ksp:metadata:modbus:modbus-ksp-c-contract")
 include(":lib:ksp:metadata:modbus:modbus-ksp-core")
-include(":lib:ksp:metadata:modbus:modbus-ksp-keil-sync")
-include(":lib:ksp:metadata:modbus:modbus-ksp-kotlin-gateway")
-include(":lib:ksp:metadata:modbus:modbus-ksp-markdown")
-include(":lib:ksp:metadata:modbus:modbus-ksp-mqtt")
 include(":lib:ksp:metadata:modbus:modbus-ksp-rtu")
 include(":lib:ksp:metadata:modbus:modbus-ksp-rtu-smoke")
 include(":lib:ksp:metadata:modbus:modbus-ksp-tcp")
 include(":lib:ksp:metadata:modbus:modbus-mqtt-gradle-plugin")
 include(":lib:ksp:metadata:modbus:modbus-rtu-gradle-plugin")
 include(":lib:ksp:metadata:modbus:modbus-runtime")
-include(":lib:ksp:metadata:modbus:modbus-tcp-gradle-plugin")
+include(":lib:tool-jvm:database:tool-sql")
+include(":lib:tool-jvm:database:tool-sql-executor")
+include(":lib:tool-jvm:database:tool-sql-injection")
+include(":lib:tool-jvm:tool-bean")
 include(":lib:tool-jvm:tool-modbus")
+include(":lib:tool-jvm:tool-s3")
 include(":lib:tool-jvm:tool-serial")
+include(":lib:tool-jvm:tool-spctx")
+include(":lib:tool-jvm:tool-spel")
+include(":lib:tool-jvm:tool-spring")
+include(":lib:tool-jvm:tool-ssh")
+include(":lib:tool-jvm:tool-stm32-bootloader")
+include(":lib:tool-kmp:tool-str")
+include(":lib:tool-starter:controller-advice-spring-boot-starter")
+include(":lib:tool-starter:controller-autoconfigure")
+include(":lib:tool-starter:curllog-spring-boot-starter")
+include(":lib:tool-starter:dict-trans-spring-boot-starter")
 // <<< Gradle Module Sleep: End Of Block <<<
